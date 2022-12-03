d7b8d1196ffdf26e809ef8535be65e6c9bf76a4a
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2022-12-03 14:40:31 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,7 +16,7 @@ class script(object):
 <i>Just send me the series name</i>''')
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """<b><i>🤖 ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/Joeyking_Bot><b>JOEY KING</b></a>\n
+    ABOUT_TXT = """<b><i>🤖 ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/Zendaya_Series_Bot><b>ZENDAYA</b></a>\n
 👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/Manlikerex><b>Manlikerex</b></a>\n
 📝 ʟᴀɴɢᴜᴀɢᴇ : ᴘʏʀᴏɢʀᴀᴍ\n
 📚 ꜰʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏᴛʜᴏɴ 3\n
